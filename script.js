a1e19deb1f136f38bdd886044fe7e9996eb806cb
Felipe Vergara <felipe.vergara.chico@gmail.com>
2023-04-14 16:03:21 -0300

    segundo requisito feito

---
+++ script.js
@@ -0,0 +1,26 @@
+const colorRed = document.getElementById('red');
+colorRed.style.backgroundColor = 'red';
+colorRed.style.border = 'solid';
+colorRed.style.width = '75%';
+colorRed.style.borderWidth = '1px';
+
+const coloGreen = document.getElementById('green');
+coloGreen.style.backgroundColor = 'green';
+coloGreen.style.border = 'solid';
+coloGreen.style.width = '75%';
+coloGreen.style.borderWidth = '1px';
+
+const colorBlack = document.getElementById('black');
+colorBlack.style.backgroundColor = 'black';
+colorBlack.style.border = 'solid';
+colorBlack.style.width = '55%';
+colorBlack.style.borderWidth = '1px';
+
+const colorBlue = document.getElementById('blue');
+colorBlue.style.backgroundColor = 'blue';
+colorBlue.style.border = 'solid';
+colorBlue.style.width = '85%';
+colorBlue.style.borderWidth = '1px';
+
+const colors = document.getElementsByClassName('colors');
+
