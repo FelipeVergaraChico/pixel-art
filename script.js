5bed6d68846945d5714bdce9befea4cc461d1a3d
Felipe Vergara <felipe.vergara.chico@gmail.com>
2024-07-09 01:33:05 -0300

    arrumando bugs antigos

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ function CoresAutomaticas() {
   for (let index = 0; index < 6; index += 1) {
     cores += letras.charAt(Math.floor(Math.random() * letras.length));
   }
-
+  console.log(cores);
   return cores;
 }
 const coresDaPagina = document.querySelectorAll('div.color');
@@ -36,96 +36,82 @@ let CoresAleatorias;
 let corAleatoria1;
 let corAleatoria3;
 const button = document.getElementById('button-random-color');
-const CoresRandom = () => { 
+const CoresRandom = () => {
   coresDaPagina[2].style.backgroundColor = CoresAutomaticas();
   coresDaPagina[3].style.backgroundColor = CoresAutomaticas();
   coresDaPagina[1].style.backgroundColor = CoresAutomaticas();
   if (CoresRandom) {
-   CoresAleatorias = coresDaPagina[2].style.backgroundColor = CoresAutomaticas();
-   corAleatoria1 = coresDaPagina[1].style.backgroundColor = CoresAutomaticas();
-   corAleatoria3 = coresDaPagina[3].style.backgroundColor = CoresAutomaticas();
+    coresDaPagina[2].style.backgroundColor = CoresAutomaticas();
+    CoresAleatorias = coresDaPagina[2].style.backgroundColor;
+    coresDaPagina[1].style.backgroundColor = CoresAutomaticas();
+    corAleatoria1 = coresDaPagina[1].style.backgroundColor;
+    coresDaPagina[3].style.backgroundColor = CoresAutomaticas();
+    corAleatoria3 = coresDaPagina[3].style.backgroundColor;
   }
-
 };
-
 
 button.addEventListener('click', CoresRandom);
 
-document.querySelector('#button-random-color').addEventListener('click', function () {
+document.querySelector('#button-random-color').addEventListener('click', () => {
   const ColorSource = CoresAleatorias;
   const ColorSource2 = corAleatoria1;
   const ColorSource3 = corAleatoria3;
   localStorage.setItem('colorPalette', JSON.stringify(ColorSource));
-  localStorage.setItem('colorPalette2',JSON.stringify(ColorSource2));
-  localStorage.setItem('colorPalette3',JSON.stringify(ColorSource3));
+  localStorage.setItem('colorPalette2', JSON.stringify(ColorSource2));
+  localStorage.setItem('colorPalette3', JSON.stringify(ColorSource3));
 });
 
+const manterACor = localStorage.getItem('colorPalette');
+const manterACor2 = localStorage.getItem('colorPalette2');
+const manterACor3 = localStorage.getItem('colorPalette3');
+const CorComJson = JSON.parse(manterACor);
+const CorComJson2 = JSON.parse(manterACor2);
+const CorComJson3 = JSON.parse(manterACor3);
 
-  const manterACor = localStorage.getItem('colorPalette');
-  const manterACor2 = localStorage.getItem('colorPalette2');
-  const manterACor3 = localStorage.getItem('colorPalette3');
-  const CorComJson = JSON.parse(manterACor);
-  const CorComJson2 = JSON.parse(manterACor2);
-  const CorComJson3 = JSON.parse(manterACor3);
+coresDaPagina[2].style.backgroundColor = CorComJson || 'blue';
+coresDaPagina[1].style.backgroundColor = CorComJson2 || 'red';
+coresDaPagina[3].style.backgroundColor = CorComJson3 || 'green';
+console.log(CorComJson);
 
-  coresDaPagina[2].style.backgroundColor = CorComJson || 'blue';
-  coresDaPagina[1].style.backgroundColor = CorComJson2 || 'red';
-  coresDaPagina[3].style.backgroundColor = CorComJson3 || 'green';
-  console.log(CorComJson);
+// fazendo requisito 6 e 7
 
-  //fazendo requisito 6 e 7
-
-  const pixel = document.getElementsByClassName('pixel');
-  for(let i = 0; i < pixel.length; i += 1) {
-    // eslint-disable-next-line no-multi-assign
-    pixel[i] += pixel[i].style.backgroundColor = 'white';
-    // pixel[i] += pixel[i].style.width = '5px';
-    // pixel[i] += pixel[i].style.height = '5x';
-  }
+const pixel = document.getElementsByClassName('pixel');
+for (let i = 0; i < pixel.length; i += 1) {
+  pixel[i].style.backgroundColor = 'white';
+  // pixel[i].style.width = '5px';
+  // pixel[i].style.height = '5x';
+}
 
 const button2 = document.getElementById('clear-board');
 
-  // requisito 9
+// requisito 9
 let ColorToDraw = 'black';
 
-const ColorSelected = (colorDraw) => {
-ColorToDraw = colorDraw;
+const ColorSelected = (e) => {
+  ColorToDraw = e.target.style.backgroundColor;
+  for (let i = 0; i < coresDaPagina.length; i += 1) {
+    coresDaPagina[i].classList.remove('selected');
+  }
+  e.target.classList.add('selected');
+};
+
+for (let i = 0; i < coresDaPagina.length; i += 1) {
+  coresDaPagina[i].addEventListener('click', ColorSelected);
 }
 
-coresDaPagina[1].addEventListener('click', () => {
-  coresDaPagina[1].classList.add('selected');
-  coresDaPagina[0].classList.remove('selected');
-  coresDaPagina[2].classList.remove('selected');
-  coresDaPagina[3].classList.remove('selected');
-});
-
-coresDaPagina[2].addEventListener('click', () => {
-  coresDaPagina[2].classList.add('selected');
-  coresDaPagina[0].classList.remove('selected');
-  coresDaPagina[1].classList.remove('selected');
-  coresDaPagina[3].classList.remove('selected');
-});
-
-coresDaPagina[3].addEventListener('click', () => {
-  coresDaPagina[3].classList.add('selected');
-  coresDaPagina[0].classList.remove('selected');
-  coresDaPagina[2].classList.remove('selected');
-  coresDaPagina[1].classList.remove('selected');
-});
-
-const Colorir = (pixel) => {
-pixel.addEventListener('click', () => {
-    pixel.style.backgroundColor = ColorToDraw;
+const Colorir = (pixels) => {
+  const pixelElement = pixels;
+  return pixelElement.addEventListener('click', () => {
+    pixelElement.style.backgroundColor = ColorToDraw;
   });
 };
 
-for(let i = 0; i < pixel.length; i += 1) {
+for (let i = 0; i < pixel.length; i += 1) {
   pixel[i] += Colorir(pixel[i]);
+}
 
+button2.addEventListener('click', () => {
+  for (let i = 0; i < pixel.length; i += 1) {
+    pixel[i].style.backgroundColor = 'white';
   }
-
-  button2.addEventListener('click', () => {
-    for(let i = 0; i < pixel.length; i += 1) {
-      pixel[i] += pixel[i].style.backgroundColor = ColorToDraw;
-    }
-  })
+});
